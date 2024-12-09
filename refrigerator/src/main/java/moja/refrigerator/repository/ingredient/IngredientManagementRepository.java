7ab95e999831764cdca12cdab7b0f2029a6025fb
yunjaeeun <yunjaeeun12@naver.com>
2024-12-09 14:57:59 +0900

    Feat: 재료관리
    1. Repository 수정
    
    Fixes: #1

--- refrigerator/src/main/java/moja/refrigerator/repository/ingredient/IngredientManagementRepository.java
+++ refrigerator/src/main/java/moja/refrigerator/repository/ingredient/IngredientManagementRepository.java
@@ -3,5 +3,5 @@ package moja.refrigerator.repository.ingredient;
 import moja.refrigerator.aggregate.ingredient.IngredientManagement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IngredientManagementRepository extends JpaRepository<Long, IngredientManagement> {
+public interface IngredientManagementRepository extends JpaRepository<IngredientManagement, Long> {
 }
