cced5b8f904d66f81544cc4ab392080fa2f808b2
김홍범 <fkdldj48@naver.com>
2024-12-10 17:28:07 +0900

    재료 ëãfeat: 록 기능 구현

--- refrigerator/src/main/java/moja/refrigerator/controller/ingredient/IngredientController.java
+++ refrigerator/src/main/java/moja/refrigerator/controller/ingredient/IngredientController.java
@@ -1,7 +1,10 @@
 package moja.refrigerator.controller.ingredient;
 
+import moja.refrigerator.dto.ingredient.request.IngredientCreateRequest;
 import moja.refrigerator.service.ingredient.IngredientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +16,10 @@ public class IngredientController {
     @Autowired
     public IngredientController(IngredientService ingredientService) {
         this.ingredientService = ingredientService;
+    }
+
+    @PostMapping
+    public void createIngredient(@RequestBody IngredientCreateRequest request) {
+        ingredientService.createIngredient(request);
     }
 }
--- refrigerator/src/main/java/moja/refrigerator/service/ingredient/IngredientService.java
+++ refrigerator/src/main/java/moja/refrigerator/service/ingredient/IngredientService.java
@@ -1,4 +1,7 @@
 package moja.refrigerator.service.ingredient;
 
+import moja.refrigerator.dto.ingredient.request.IngredientCreateRequest;
+
 public interface IngredientService {
+    void createIngredient(IngredientCreateRequest request);
 }
