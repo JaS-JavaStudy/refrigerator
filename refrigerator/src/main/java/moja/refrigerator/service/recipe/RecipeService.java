e7cf93ebdaa3f7007c1024d606d2b6a8af56560d
mincheol <kmc4889@naver.com>
2024-12-11 18:36:21 +0900

    [김민철] 한 일 : 1. cooking_source -> recipe_source 로 일괄 변경. 2. 생성용 Category, Repository, Request, 추가 3. 읽기 용 Repository, response 추가 4. UserDummyRepository 생성 5. 임시 recipe_source_type, recipe_source_category 추가
    문제점 :
    1. recipe에 recipe 적을 란이 없다.
    2. RecipeCreateTime이 저장되는데 format을 안된다.
    
    해야할 일 :
    1. 문제점 수정
    2. UD 생성

--- refrigerator/src/main/java/moja/refrigerator/service/recipe/RecipeService.java
+++ refrigerator/src/main/java/moja/refrigerator/service/recipe/RecipeService.java
@@ -1,4 +1,14 @@
 package moja.refrigerator.service.recipe;
 
+import moja.refrigerator.aggregate.recipe.Recipe;
+import moja.refrigerator.dto.recipe.request.RecipeCreateRequest;
+import moja.refrigerator.dto.recipe.response.RecipeResponse;
+import moja.refrigerator.repository.recipe.RecipeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 public interface RecipeService {
+    void createRecipe(RecipeCreateRequest request);
+    List<RecipeResponse> getAllRecipes();
 }
