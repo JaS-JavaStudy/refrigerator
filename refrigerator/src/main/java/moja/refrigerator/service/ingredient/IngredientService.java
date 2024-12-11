c778f4b1031d56674b2b946498313e90c9ce548d
김홍범 <fkdldj48@naver.com>
2024-12-11 16:07:32 +0900

    #1 재료 조회 기능 구현

--- refrigerator/src/main/java/moja/refrigerator/service/ingredient/IngredientService.java
+++ refrigerator/src/main/java/moja/refrigerator/service/ingredient/IngredientService.java
@@ -1,7 +1,11 @@
 package moja.refrigerator.service.ingredient;
 
 import moja.refrigerator.dto.ingredient.request.IngredientCreateRequest;
+import moja.refrigerator.dto.ingredient.response.IngredientResponse;
+
+import java.util.List;
 
 public interface IngredientService {
-    void createIngredient(IngredientCreateRequest request);
+    void createIngredient(IngredientCreateRequest request); // 재료 등록 메서드
+    List<IngredientResponse> getIngredient(); // 재료 조회 메서드
 }
